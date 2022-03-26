3dcf4fbd7fde8aed9331b5a1cce359c8ff30d511
Rajveer Singh <rsingh@workforcelogiq.com>
2022-03-26 21:35:15 +0530

    Filter Page Pop Window

--- components/Views/FilterPage.js
+++ components/Views/FilterPage.js
@@ -1,16 +1,25 @@
 import { View, Text, Dimensions } from 'react-native';
+import React, { useState } from 'react';
 export default function FilterPage(props) {
     if (!props.isFilterPageVisible) {
         return null;
     }
-    const windowWidth = Dimensions.get('window').width;
-    const windowHeight = Dimensions.get('window').height;
-    const tostMessageBGColor = props.type == 'info' ? 'green' : 'red';
+    const [windowWidth, setWindowWidth] = useState(Dimensions.get('window').width);
+    const [windowHeight, setWindowHeight] = useState(Dimensions.get('window').height);
+    const [modelWindowWidth, setModelWindowWidth] = useState(Dimensions.get('window').width / 2);
+    const [modelWindowHeight, setModelWindowHeight] = useState(Dimensions.get('window').height / 2);
+    //Handle Screen Orientation
+    Dimensions.addEventListener('change', () => {
+        setWindowWidth(Dimensions.get('window').width);
+        setWindowHeight(Dimensions.get('window').height);
+        setModelWindowWidth(Dimensions.get('window').width / 2);
+        setModelWindowHeight(Dimensions.get('window').height / 2);
+    });
 
     return (
-        <View style={{ display: 'flex', backgroundColor: 'transparent', position: 'absolute', flex: 1, width: windowWidth, height: windowHeight, justifyContent: 'center', alignItems: 'center' }}>
-            <View style={{ borderRadius: 5, justifyContent: 'centre', alignItems: 'center', backgroundColor: tostMessageBGColor, padding: 30, flex: 1 }}>
-                <Text style={{ color: 'white', fontSize: 20 }}>Filter Page</Text>
+        <View style={{ display: 'flex', position: 'absolute', flex: 1, width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0,0,0,0.7)' }}>
+            <View style={{ borderRadius: 5, backgroundColor: 'white', flex: 1, width: '80%', height: '90%', margin: 50, position: 'absolute', justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={{ color: 'black', fontSize: 20 }}>Filter Page Pop-up Window</Text>
             </View>
         </View>
     )
