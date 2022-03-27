4a8649d3ebb85efd49eeb91b82105a6894c87407
Rajveer Singh <rsingh@workforcelogiq.com>
2022-03-27 09:13:21 +0530

    Code Cleanup

--- components/Views/FilterPage.js
+++ components/Views/FilterPage.js
@@ -1,20 +1,10 @@
-import { View, Text, Dimensions } from 'react-native';
+import { View, Text } from 'react-native';
 import React, { useState } from 'react';
 export default function FilterPage(props) {
     if (!props.isFilterPageVisible) {
         return null;
     }
-    const [windowWidth, setWindowWidth] = useState(Dimensions.get('window').width);
-    const [windowHeight, setWindowHeight] = useState(Dimensions.get('window').height);
-    const [modelWindowWidth, setModelWindowWidth] = useState(Dimensions.get('window').width / 2);
-    const [modelWindowHeight, setModelWindowHeight] = useState(Dimensions.get('window').height / 2);
-    //Handle Screen Orientation
-    Dimensions.addEventListener('change', () => {
-        setWindowWidth(Dimensions.get('window').width);
-        setWindowHeight(Dimensions.get('window').height);
-        setModelWindowWidth(Dimensions.get('window').width / 2);
-        setModelWindowHeight(Dimensions.get('window').height / 2);
-    });
+
 
     return (
         <View style={{ display: 'flex', position: 'absolute', flex: 1, width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0,0,0,0.7)' }}>
